cba9405afbad5e94ad42fbb06da126fd9d48b494
HakanTaskinerr <HakanTaskinerr@users.noreply.github.com>
2020-07-02 17:08:23 +0300

    Update Game.py

--- Game.py
+++ Game.py
@@ -11,41 +11,53 @@ root.resizable(width = False, height = False)
 
 RockPhoto = PhotoImage(file = r'C:\Users\Hakan\Desktop\Rock-Scissors-Paper\Images\ROCK.png')
 PaperPhoto = PhotoImage(file = r'C:\Users\Hakan\Desktop\Rock-Scissors-Paper\Images\PAPER.png')
-ScissorsPhoto = PhotoImage(file = r'C:\Users\Hakan\Desktop\Rock-Scissors-Paper\Images\SCISSORS.PNG')
+ScissorsPhoto = PhotoImage(file = r'C:\Users\Hakan\Desktop\Rock-Scissors-Paper\Images\SCISSORS.png')
 
 image_list = [RockPhoto, PaperPhoto, ScissorsPhoto]
- 
+
+mylabel = Label(root, text = "")
+computer_label = Label(root, text = "")
 
 def select_rock():
-    mylabel = Label(root, image = image_list[0]).grid(row = 6, column = 2)
+    global mylabel
     mylabel.grid_forget()
-    mylabel.delete()
+    mylabel.destroy()
+    mylabel = Label(root, image = image_list[0])
+    mylabel.grid(row = 6, column = 2)
 
 def select_paper():
-    mylabel = Label(root, image = image_list[1]).grid(row = 6, column = 2)
+    global mylabel
     mylabel.grid_forget()
-    mylabel.delete()
+    mylabel.destroy()
+    mylabel = Label(root, image = image_list[1])
+    mylabel.grid(row = 6, column = 2)
 
 def select_scissors():
-    mylabel = Label(root, image = image_list[2]).grid(row = 6, column = 2)
+    global mylabel
     mylabel.grid_forget()
-    mylabel.delete()
+    mylabel.destroy()
+    mylabel = Label(root, image = image_list[2])
+    mylabel.grid(row = 6, column = 2)
 
 def Computer_pick():
-    computer_label = Label(root, image = random.choice(image_list)).grid(row = 6, column = 1)
+    global computer_label
+    computer_label.grid_forget()
+    computer_label.destroy()
+    computer_label = Label(root, image = random.choice(image_list))
+    computer_label.grid(row = 6, column = 0)
 
+    
+    
+ 
 
+Rock_Button = Button(root, image = RockPhoto, command = select_rock)
+Rock_Button.grid(row = 0, column = 0)
+Paper_Button = Button(root, image = PaperPhoto, command = select_paper)
+Paper_Button.grid(row = 0, column = 1)
+Scissors_Button = Button(root, image = ScissorsPhoto, command = select_scissors)
+Scissors_Button.grid(row = 0, column = 2)
 
-
-
-
-Rock_Button = Button(root, image = RockPhoto, command = lambda : select_rock()).grid(row = 0, column = 0)
-Paper_Button = Button(root, image = PaperPhoto, command = lambda : select_paper()).grid(row = 0, column = 1)
-Scissors_Button = Button(root, image = ScissorsPhoto, command = lambda : select_scissors()).grid(row = 0, column = 2)
-
-Computer_Button = Button(root, text = "Computer's Choice", command = lambda : Computer_pick()).grid(row = 5, column = 1)
-
-
+Computer_Button = Button(root, text = "Computer's Choice", command = Computer_pick).grid(row = 5, column = 1)
 
 
 
