17f3cb578928376b31e2b9dcd2e42efeaa47c85c
HakanTaskinerr <HakanTaskinerr@users.noreply.github.com>
2020-07-02 14:39:04 +0300

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from PIL import Image, ImageTk
+import random
 
 root = Tk()
 
@@ -7,6 +8,12 @@ root.iconbitmap(r'C:\Users\Hakan\Desktop\Hakan\image\icon.ico')
 root.title('ROCK SCISSORS PAPER')
 root.geometry("600x700")
 root.resizable(width = False, height = False)
+
+game_list = [
+    "Rock",
+    "Paper",
+    "Scissors"
+]
 
 def select_rock():
     mylabel = Label(root, text = "Rock").grid(row = 1, column = 0)
@@ -17,6 +24,9 @@ def select_paper():
 def select_scissors():
     mylabel = Label(root, text = "Scissors").grid(row = 1, column = 2)
 
+def Computer_pick():
+    computer_label = Label(root, text = random.choice(game_list)).grid(row = 3, column = 0)
+
 
 RockPhoto = PhotoImage(file = r'C:\Users\Hakan\Desktop\Rock-Scissors-Paper\Images\ROCK.png')
 PaperPhoto = PhotoImage(file = r'C:\Users\Hakan\Desktop\Rock-Scissors-Paper\Images\PAPER.png')
@@ -25,6 +35,8 @@ ScissorsPhoto = PhotoImage(file = r'C:\Users\Hakan\Desktop\Rock-Scissors-Paper\I
 Rock_Button = Button(root, image = RockPhoto, command = lambda : select_rock()).grid(row = 0, column = 0)
 Paper_Button = Button(root, image = PaperPhoto, command = lambda : select_paper()).grid(row = 0, column = 1)
 Scissors_Button = Button(root, image = ScissorsPhoto, command = lambda : select_scissors()).grid(row = 0, column = 2)
+
+Computer_Button = Button(root, text = "Computer's Choice", command = lambda : Computer_pick()).grid(row = 5, column = 1)
 
 
 
