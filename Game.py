d76a3e3556035aa289fdbba615227d0584467826
HakanTaskinerr <HakanTaskinerr@users.noreply.github.com>
2020-07-02 14:29:59 +0300

    Update Game.py

--- Game.py
+++ Game.py
@@ -5,14 +5,26 @@ root = Tk()
 
 root.iconbitmap(r'C:\Users\Hakan\Desktop\Hakan\image\icon.ico')
 root.title('ROCK SCISSORS PAPER')
+root.geometry("600x700")
+root.resizable(width = False, height = False)
+
+def select_rock():
+    mylabel = Label(root, text = "Rock").grid(row = 1, column = 0)
+
+def select_paper():
+    mylabel = Label(root, text = "Paper").grid(row = 1, column = 1)
+
+def select_scissors():
+    mylabel = Label(root, text = "Scissors").grid(row = 1, column = 2)
+
 
 RockPhoto = PhotoImage(file = r'C:\Users\Hakan\Desktop\Rock-Scissors-Paper\Images\ROCK.png')
 PaperPhoto = PhotoImage(file = r'C:\Users\Hakan\Desktop\Rock-Scissors-Paper\Images\PAPER.png')
 ScissorsPhoto = PhotoImage(file = r'C:\Users\Hakan\Desktop\Rock-Scissors-Paper\Images\SCISSORS.PNG')
 
-my_button = Button(root, image = RockPhoto).grid(row = 0, column = 0)
-my_button2 = Button(root, image = PaperPhoto).grid(row = 0, column = 1)
-my_button3 = Button(root, image = ScissorsPhoto).grid(row = 0, column = 2)
+Rock_Button = Button(root, image = RockPhoto, command = lambda : select_rock()).grid(row = 0, column = 0)
+Paper_Button = Button(root, image = PaperPhoto, command = lambda : select_paper()).grid(row = 0, column = 1)
+Scissors_Button = Button(root, image = ScissorsPhoto, command = lambda : select_scissors()).grid(row = 0, column = 2)
 
 
 
