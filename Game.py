b9a673774e3868da7a9bd669ad7544a537ca908e
HakanTaskinerr <HakanTaskinerr@users.noreply.github.com>
2020-07-02 14:18:16 +0300

    Update Game.py

--- Game.py
+++ Game.py
@@ -6,6 +6,10 @@ root = Tk()
 root.iconbitmap(r'C:\Users\Hakan\Desktop\Hakan\image\icon.ico')
 root.title('ROCK SCISSORS PAPER')
 
+RockPhoto = PhotoImage(file = r'C:\Users\Hakan\Desktop\Rock-Scissors-Paper\Images\ROCK.png')
+PaperPhoto = PhotoImage(file = r'C:\Users\Hakan\Desktop\Rock-Scissors-Paper\Images\PAPER.png')
+ScissorsPhoto = PhotoImage(file = r'C:\Users\Hakan\Desktop\Rock-Scissors-Paper\Images\SCISSORS.PNG')
+
 
 
 
