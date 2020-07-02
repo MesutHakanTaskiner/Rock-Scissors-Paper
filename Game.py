a7a4f9f2658b41a68124e7c4a3946300f8e84d1c
HakanTaskinerr <HakanTaskinerr@users.noreply.github.com>
2020-07-02 14:16:10 +0300

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,2 +1,17 @@
 from tkinter import *
 from PIL import Image, ImageTk
+
+root = Tk()
+
+root.iconbitmap(r'C:\Users\Hakan\Desktop\Hakan\image\icon.ico')
+root.title('ROCK SCISSORS PAPER')
+
+
+
+
+
+
+
+
+
+root.mainloop()
