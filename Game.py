c4a11ad65363a2a4eaa7934e3e8a4e6b3ca6882f
HakanTaskinerr <HakanTaskinerr@users.noreply.github.com>
2020-07-02 16:06:57 +0300

    Update Game.py

--- Game.py
+++ Game.py
@@ -6,31 +6,38 @@ root = Tk()
 
 root.iconbitmap(r'C:\Users\Hakan\Desktop\Hakan\image\icon.ico')
 root.title('ROCK SCISSORS PAPER')
-root.geometry("600x700")
+root.geometry("800x900")
 root.resizable(width = False, height = False)
-
-game_list = [
-    "Rock",
-    "Paper",
-    "Scissors"
-]
-
-def select_rock():
-    mylabel = Label(root, text = "Rock").grid(row = 1, column = 0)
-
-def select_paper():
-    mylabel = Label(root, text = "Paper").grid(row = 1, column = 1)
-
-def select_scissors():
-    mylabel = Label(root, text = "Scissors").grid(row = 1, column = 2)
-
-def Computer_pick():
-    computer_label = Label(root, text = random.choice(game_list)).grid(row = 3, column = 0)
-
 
 RockPhoto = PhotoImage(file = r'C:\Users\Hakan\Desktop\Rock-Scissors-Paper\Images\ROCK.png')
 PaperPhoto = PhotoImage(file = r'C:\Users\Hakan\Desktop\Rock-Scissors-Paper\Images\PAPER.png')
 ScissorsPhoto = PhotoImage(file = r'C:\Users\Hakan\Desktop\Rock-Scissors-Paper\Images\SCISSORS.PNG')
+
+image_list = [RockPhoto, PaperPhoto, ScissorsPhoto]
+ 
+
+def select_rock():
+    mylabel = Label(root, image = image_list[0]).grid(row = 6, column = 2)
+    mylabel.grid_forget()
+    mylabel.delete()
+
+def select_paper():
+    mylabel = Label(root, image = image_list[1]).grid(row = 6, column = 2)
+    mylabel.grid_forget()
+    mylabel.delete()
+
+def select_scissors():
+    mylabel = Label(root, image = image_list[2]).grid(row = 6, column = 2)
+    mylabel.grid_forget()
+    mylabel.delete()
+
+def Computer_pick():
+    computer_label = Label(root, image = random.choice(image_list)).grid(row = 6, column = 1)
+
+
+
+
+
 
 Rock_Button = Button(root, image = RockPhoto, command = lambda : select_rock()).grid(row = 0, column = 0)
 Paper_Button = Button(root, image = PaperPhoto, command = lambda : select_paper()).grid(row = 0, column = 1)
