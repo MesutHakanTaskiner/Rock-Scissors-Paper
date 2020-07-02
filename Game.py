91f3bba4c43a889d08f0828141ed5d3d36d206aa
HakanTaskinerr <HakanTaskinerr@users.noreply.github.com>
2020-07-02 14:19:53 +0300

    Update Game.py

--- Game.py
+++ Game.py
@@ -10,6 +10,9 @@ RockPhoto = PhotoImage(file = r'C:\Users\Hakan\Desktop\Rock-Scissors-Paper\Image
 PaperPhoto = PhotoImage(file = r'C:\Users\Hakan\Desktop\Rock-Scissors-Paper\Images\PAPER.png')
 ScissorsPhoto = PhotoImage(file = r'C:\Users\Hakan\Desktop\Rock-Scissors-Paper\Images\SCISSORS.PNG')
 
+my_button = Button(root, image = RockPhoto).grid(row = 0, column = 0)
+my_button2 = Button(root, image = PaperPhoto).grid(row = 0, column = 1)
+my_button3 = Button(root, image = ScissorsPhoto).grid(row = 0, column = 2)
 
 
 
