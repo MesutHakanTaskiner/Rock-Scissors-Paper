dcebefc07017f2cf919ade8577443abef4ba4991
HakanTaskinerr <HakanTaskinerr@users.noreply.github.com>
2020-07-03 13:34:01 +0300

    Update Game.py

--- Game.py
+++ Game.py
@@ -22,13 +22,14 @@ computer = ""
 mylabel = Label(root, text = "")
 computer_label = Label(root, text = "")
 
+
 def select_rock():
     global mylabel
     global user
 
     mylabel.grid_forget()
     mylabel.destroy()
-    
+
     mylabel = Label(root, image = image_list[0])
     mylabel.grid(row = 2, column = 0)
 
@@ -88,20 +89,44 @@ def Computer_pick():
     mylabel3 = Label(root, text = "COMPUTER", fg = "White", bg = "Black")
     mylabel3.grid(row = 7, column = 2)
 
-if user == 'pyimage1' and computer == 'pyimage2':
-    label = Label(root, text = "User Win")
-    label.grid(row = 8, column = 1)
+if user == "pyimage1" and computer == "pyimage2":
+        label = Label(root, text = "Computer Won")
+        label.grid(row = 8, column = 3)
+elif user == "pyimage1" and computer == "pyimage3":
+        label2 = Label(root, text = "User Won")
+        label.grid(row = 8, column = 1)
+elif user == "pyimage2" and computer == "pyimage3":
+        label3 = Label(root, text = "Computer Won")
+        label.grid(row = 8, column = 1)
+elif user == "pyimage2" and computer == "pyimage1":
+        label4 = Label(root, text = "User Won")
+        label.grid(row = 8, column = 1)
+elif user == "pyimage3" and computer == "pyimage1":
+        label5 = Label(root, text = "Computer Won")
+        label.grid(row = 8, column = 1)
+elif user == "pyimage3" and computer == "pyimage2":
+        label6 = Label(root, text = "User Won")
+        label.grid(row = 8, column = 1)
+elif user == "pyimage1" and computer == "pyimage1":
+        label7 = Label(root, text = "Draw")
+        label.grid(row = 8, column = 1)
+elif user == "pyimage2" and computer == "pyimage2":
+        label8 = Label(root, text = "Draw")
+        label.grid(row = 8, column = 1)
+elif user == "pyimage3" and computer == "pyimage1":
+        label9 = Label(root, text = "Draw")
+        label.grid(row = 8, column = 1)
+
 
 
 
 Rock_Button = Button(root, text = "Rock", command = select_rock)
 Rock_Button.grid(row = 0, column = 0)
+
 Paper_Button = Button(root, text = "Paper", command = select_paper)
 Paper_Button.grid(row = 0, column = 1)
+
 Scissors_Button = Button(root, text = "Scissors", command = select_scissors)
 Scissors_Button.grid(row = 0, column = 2)
-
-#Computer_Button = Button(root, text = "Computer's Choice", command = Computer_pick).grid(row = 5, column = 1)
-
 
 root.mainloop()
