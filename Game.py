24a12153c84b7c375563508b88bac78e204db9c8
HakanTaskinerr <HakanTaskinerr@users.noreply.github.com>
2020-07-03 03:18:00 +0300

    Update Game.py

--- Game.py
+++ Game.py
@@ -6,6 +6,7 @@ root = Tk()
 
 root.iconbitmap(r'C:\Users\Hakan\Desktop\Hakan\image\icon.ico')
 root.title('ROCK SCISSORS PAPER')
+root.geometry("700x700")
 
 #root.resizable(width = False, height = False)
 
@@ -15,47 +16,66 @@ ScissorsPhoto = PhotoImage(file = r'C:\Users\Hakan\Desktop\Rock-Scissors-Paper\I
 
 image_list = [RockPhoto, PaperPhoto, ScissorsPhoto]
 
+user = ""
+computer = ""
+
 mylabel = Label(root, text = "")
 computer_label = Label(root, text = "")
 
 def select_rock():
     global mylabel
+    global user
     mylabel.grid_forget()
     mylabel.destroy()
     mylabel = Label(root, image = image_list[0])
     mylabel.grid(row = 2, column = 0)
+    user = "pyimage1"
     Computer_pick()
-    mylabel2 = Label(root, text = "PLAYER", fg = "White", bg = "Black")
+    mylabel2 = Label(root, text = "PLAYER", fg = "White", bg = "Black") 
     mylabel2.grid(row = 7, column = 0)
 
 def select_paper():
     global mylabel
+    global user
     mylabel.grid_forget()
     mylabel.destroy()
     mylabel = Label(root, image = image_list[1])
     mylabel.grid(row = 2, column = 0)
+    user = "pyimage2"
     Computer_pick()
     mylabel2 = Label(root, text = "PLAYER", fg = "White", bg = "Black")
     mylabel2.grid(row = 7, column = 0)  
 
 def select_scissors():
     global mylabel
+    global user
     mylabel.grid_forget()
     mylabel.destroy()
     mylabel = Label(root, image = image_list[2])
     mylabel.grid(row = 2, column = 0)
+    user = "pyimage3"
     Computer_pick()
     mylabel2 = Label(root, text = "PLAYER", fg = "White", bg = "Black")
     mylabel2.grid(row = 7, column = 0)
 
 def Computer_pick():
     global computer_label
+    global computer
     computer_label.grid_forget()
     computer_label.destroy()
-    computer_label = Label(root, image = random.choice(image_list))
-    computer_label.grid(row = 2, column = 2)
+    computer = random.choice(image_list)
+    computer_label = Label(root, image = computer)
+    computer_label.grid(row = 2, column = 2)    
+
     mylabel3 = Label(root, text = "COMPUTER", fg = "White", bg = "Black")
     mylabel3.grid(row = 7, column = 2)
+
+    print(computer)
+if user == 'pyimage1' and computer == 'pyimage2':
+    label = Label(root, text = "User Win")
+    print("dsfgds")
+    label.grid(row = 8, column = 1)
+
 
 
 Rock_Button = Button(root, text = "Rock", command = select_rock)
@@ -66,10 +86,6 @@ Scissors_Button = Button(root, text = "Scissors", command = select_scissors)
 Scissors_Button.grid(row = 0, column = 2)
 
 #Computer_Button = Button(root, text = "Computer's Choice", command = Computer_pick).grid(row = 5, column = 1)
-
-
-
-
 
 
 root.mainloop()
